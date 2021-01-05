4fbef92bba39479835722174ca379dc13919ca04
yuu <shiratsuyuclassyuudachi@qdacm.com>
2021-01-05 17:34:16 +0800

    add classify

--- test/upload.js
+++ test/upload.js
@@ -1,6 +1,5 @@
 $(document).ready(function (){
     $("#upload").click(function (){
-        var path;
         var img = document.querySelector("input[type='file']");
         var formdata = new FormData();
         formdata.append("file",img.files[0]);
@@ -10,13 +9,32 @@ $(document).ready(function (){
             body: formdata,
             redirect: 'follow'
         };
-        for(let i=0;i<5;i++){
-            fetch("http://172.26.36.228/api/images/upload", requestOptions)
-                .then(response=>response.json().then(function (json){
-                    var path = json.path
-                    console.log(path);
-                    break;
-            }))
-        }
+        fetch("http://172.26.36.228/api/images/upload", requestOptions)
+        .then(response=>{
+            if(response.status===201){
+                response.json().then(function (json){
+                    json.forEach(element =>{
+                        var path = element.path;
+                        var myHeaders = new Headers();
+                        myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+                        var urlencoded = new URLSearchParams();
+                        urlencoded.append("fpath", path);
+                        var reqOptions = {
+                            method: 'POST',
+                            headers: myHeaders,
+                            body: urlencoded,
+                            redirect: 'follow'
+                        };
+                        fetch("http://172.26.36.228/api/images/classify", reqOptions)
+                        .then(response => {
+                            if(response.status===200){
+                                $("#result").val(response.text());
+                            }
+                        })
+                        .catch(error => console.log('error', error));
+                    })
+                })
+            }
+        })
     })
 })
